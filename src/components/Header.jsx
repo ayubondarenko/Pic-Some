6f622a42bca45193029ceebd22a584d2348d8974
Alex <ayubondarenko@yandex.ru>
2020-01-21 21:15:36 +0700

    ginish Challenge1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
-	return (<header>
-		<h2>
-			<Link to="/">
-				Pic Some
-			</Link>
-		</h2>
-		<Link to="/cart">
-			<i className="ri-shopping-cart-line ri-fw ri-2x"/>
-		</Link>
-	</header>);
+  return (
+    <header>
+      <Link to="/">
+        <h2>Pic Some</h2>
+      </Link>
+      <Link to="/cart">
+        <i className="ri-shopping-cart-line ri-fw ri-2x" />
+      </Link>
+    </header>
+  );
 }
 
 export default Header;

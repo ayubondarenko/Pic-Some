f86acca9909f4be4461a899f72397d7af150bd9b
Alexander Bondarenko <a.bondarenko@staffcop.ru>
2020-01-22 18:12:16 +0700

    preffifer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import {hot} from 'react-hot-loader/root';
+import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { ContextProvider } from './context';
 import Header from './components/Header.jsx';
 import Cart from './pages/Cart.jsx';
 import Photos from './pages/Photos';
 
 function App() {
-	return (<div>
-		<Header/>
-		<Switch>
-			<Route exact path="/"><Photos/></Route>
-			<Route path="/"><Cart/></Route>
-		</Switch>
-	</div>);
+  return (
+    <ContextProvider>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Photos />
+            </Route>
+            <Route path="/">
+              <Cart />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </ContextProvider>
+  );
 }
 
 export default hot(App);
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,7 @@
-import "./styles.css";
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { ContextProvider } from "./context";
-import App from "./App";
+import './styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <ContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ContextProvider>,
-  document.getElementById("root")
-);
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
